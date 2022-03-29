47ead9fb5ad025f5a9be7dc491e588334c886341
RaymondLaubert <59926120+RaymondLaubert@users.noreply.github.com>
2022-03-29 13:33:25 -0400

    Updated main.py for Deployment to Server
    Changed "app.run(debug=True)" to "app.run(host='0.0.0.0', port=5000)" so that it would work with the deployment to the AWS EC2 Server.

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ app = create_app()
 app.app_context().push()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
     #run flask application
